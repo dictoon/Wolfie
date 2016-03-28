45fc7ea6956c87a1bb467aaae84f6845f38f9a82
Francois Beaune <beaune@aist.enst.fr>
2016-03-28 12:55:41 +0200

    Robust ray casting

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+#define myassert(cond) if (!(cond)) { __debugbreak(); }
+
 const size_t ScreenWidth = 800;
 const size_t ScreenHeight = 600;
 
@@ -26,10 +28,21 @@ ScreenPixel rgb(uint8_t r, uint8_t g, uint8_t b)
     return { b, g, r, 255 };
 }
 
-ScreenPixel rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+#if 0
+
+const size_t MapW = 4;
+const size_t MapH = 4;
+
+// (0,0) at bottom left.
+const uint8_t Map[MapW * MapH] =
 {
-    return { b, g, r, a };
-}
+    1, 1, 1, 1,
+    1, 0, 0, 1,
+    1, 0, 0, 1,
+    1, 1, 1, 1
+};
+
+#else
 
 const size_t MapW = 8;
 const size_t MapH = 8;
@@ -47,9 +60,11 @@ const uint8_t Map[MapW * MapH] =
     1, 1, 1, 1, 1, 1, 1, 1
 };
 
+#endif
+
 uint8_t map(const int ix, const int iy)
 {
-    assert(
+    myassert(
         ix >= 0 &&
         iy >= 0 &&
         ix <= static_cast<int>(MapW) - 1 &&
@@ -59,7 +74,7 @@ uint8_t map(const int ix, const int iy)
         iy >= 0 &&
         ix <= static_cast<int>(MapW) - 1 &&
         iy <= static_cast<int>(MapH) - 1
-            ? Map[(MapH - iy - 1) * MapW + ix]
+            ? Map[(MapH - 1 - iy) * MapW + ix]
             : 1;
 }
 
@@ -67,7 +82,7 @@ const float Pi = 3.1415926535897932f;
 const float HFov = 90.0f * Pi / 180.0f;
 const float VFov = HFov * ScreenHeight / ScreenWidth;
 const float PlayerMoveSpeed = 0.03f;
-const float PlayerRotateSpeed = 3.0f;
+const float PlayerRotateSpeed = 3.0f * Pi / 180.0f;
 const float FilmWidth = 0.01f;
 const float FilmHeight = FilmWidth * ScreenHeight / ScreenWidth;
 const float FocalLength = FilmWidth / 2.0f * tan(HFov / 2.0f);
@@ -83,9 +98,9 @@ Player player;
 
 void init()
 {
-    player.x = 2.1f;
-    player.y = 2.1f;
-    player.a = 0.0f;
+    player.x = 2.0f;
+    player.y = 2.0f;
+    player.a = 180.0f * Pi / 180.0f;
 }
 
 bool cast_ray(
@@ -93,58 +108,103 @@ bool cast_ray(
     const float x1, const float y1,
     float* hx, float* hy)
 {
-    int ix = static_cast<int>(floor(x0));
-    int iy = static_cast<int>(floor(y0));
+    myassert(x0 >= 0.0f && x0 < static_cast<float>(MapW));
+    myassert(y0 >= 0.0f && y0 < static_cast<float>(MapH));
+
     float x = x0;
     float y = y0;
+
+    int ix = static_cast<int>(x);
+    int iy = static_cast<int>(y);
+
+    if (x == ix && x1 < x0)
+        ix -= 1;
+
+    if (y == iy && y1 < y0)
+        iy -= 1;
+
+    myassert(map(ix, iy) == 0);
+
+    const int ix1 = static_cast<int>(x1);
+    const int iy1 = static_cast<int>(y1);
 
     while (true)
     {
         const float dx = x1 - x;
         const float dy = y1 - y;
 
-        // Intersect vertical walls.
-        float tx = Infinity;
-        if (dx != 0.0f)
+        float wallx, tx;
+        if (dx > 0.0f)
         {
-            const float wallx = dx > 0.0f ? ix + 1.0f : ix;
-            const float t = (wallx - x) / dx;
-            assert(t >= 0.0f);
-            if (t > 0.0f && t <= 1.0f)
-                tx = t;
-        }
+            wallx = ceil(x);
+            if (x == wallx)
+                wallx += 1.0f;
+            myassert(x < wallx);
 
-        // Intersect horizontal walls.
-        float ty = Infinity;
-        if (dy != 0.0f)
-        {
-            const float wally = dy > 0.0f ? iy + 1.0f : iy;
-            const float t = (wally - y) / dy;
-            assert(t >= 0.0f);
-            if (t > 0.0f && t <= 1.0f)
-                ty = t;
+            tx = (wallx - x) / dx;
+            myassert(tx > 0.0f);
         }
+        else if (dx < 0.0f)
+        {
+            wallx = floor(x);
+            if (x == wallx)
+                wallx -= 1.0f;
+            myassert(x > wallx);
+
+            tx = (wallx - x) / dx;
+            myassert(tx > 0.0f);
+        }
+        else tx = Infinity;
+
+
+        float wally, ty;
+        if (dy > 0.0f)
+        {
+            wally = ceil(y);
+            if (y == wally)
+                wally += 1.0f;
+            myassert(y < wally);
+
+            ty = (wally - y) / dy;
+            myassert(ty > 0.0f);
+        }
+        else if (dy < 0.0f)
+        {
+            wally = floor(y);
+            if (y == wally)
+                wally -= 1.0f;
+            myassert(y > wally);
+
+            ty = (wally - y) / dy;
+            myassert(ty > 0.0f);
+        }
+        else ty = Infinity;
+
+
+        myassert(tx < ty || ty < tx);
 
         if (tx < ty)
         {
-            // Hit vertical wall first.
-            x += tx * dx;
+            x = wallx;
             y += tx * dy;
-            ix += dx > 0.0f ? 1 : -1;
-            if (map(ix, iy))
+
+            myassert(dx != 0.0f);
+            if (dx > 0.0f)
             {
-                *hx = x;
-                *hy = y;
-                return true;
+                ix += 1;
+                myassert(ix == static_cast<int>(x));
+                myassert(ix <= static_cast<int>(MapW - 1));
+                iy = static_cast<int>(y);
             }
-        }
-        else if (ty < tx)
-        {
-            // Hit horizontal wall first.
-            x += ty * dx;
-            y += ty * dy;
-            iy += dy > 0.0f ? 1 : -1;
-            if (map(ix, iy))
+            else
+            {
+                ix -= 1;
+                //myassert(ix == static_cast<int>(x));
+                myassert(ix >= 0);
+                iy = static_cast<int>(y);
+            }
+
+            if (map(ix, iy) != 0)
             {
                 *hx = x;
                 *hy = y;
@@ -153,9 +213,35 @@ bool cast_ray(
         }
         else
         {
-            // No hit.
-            return false;
+            x += ty * dx;
+            y = wally;
+
+            myassert(dy != 0.0f);
+            if (dy > 0.0f)
+            {
+                iy += 1;
+                myassert(iy == static_cast<int>(y));
+                myassert(iy <= static_cast<int>(MapH - 1));
+                ix = static_cast<int>(x);
+            }
+            else
+            {
+                iy -= 1;
+                //myassert(iy == static_cast<int>(y));
+                myassert(iy >= 0);
+                ix = static_cast<int>(x);
+            }
+
+            if (map(ix, iy) != 0)
+            {
+                *hx = x;
+                *hy = y;
+                return true;
+            }
         }
+
+        if (ix == ix1 && iy == iy1)
+            return false;
     }
 }
 
@@ -163,45 +249,84 @@ void update()
 {
     const uint8_t* keys = SDL_GetKeyboardState(nullptr);
 
-    const float rotate =
-        keys[SDL_SCANCODE_LEFT] ? +PlayerRotateSpeed :
-        keys[SDL_SCANCODE_RIGHT] ? -PlayerRotateSpeed : 0.0f;
+    float dx = 0.0f, dy = 0.0f;
 
-    const float move =
-        keys[SDL_SCANCODE_UP] ? +PlayerMoveSpeed :
-        keys[SDL_SCANCODE_DOWN] ? -PlayerMoveSpeed : 0.0f;
-
-    player.a += rotate * Pi / 180.0f;
-
-    if (move != 0.0f)
+    if (keys[SDL_SCANCODE_LEFT])
     {
-        const float dx = move * cos(player.a);
-        const float dy = move * sin(player.a);
-
-        player.x += dx;
-        player.y += dy;
-
-        const float Margin = 0.2f;
-        float hx, hy;
-
-        const float sensex = player.x + (dx > 0.0f ? Margin : -Margin);
-        if (cast_ray(
-                player.x, player.y,
-                sensex, player.y,
-                &hx, &hy))
+        if (keys[SDL_SCANCODE_LALT])
         {
-            player.x -= sensex - hx;
+            dx += PlayerMoveSpeed * cos(player.a + Pi / 2.0f);
+            dy += PlayerMoveSpeed * sin(player.a + Pi / 2.0f);
         }
+        else player.a += PlayerRotateSpeed;
+    }
 
-        const float sensey = player.y + (dy > 0.0f ? Margin : -Margin);
-        if (cast_ray(
-                player.x, player.y,
-                player.x, sensey,
-                &hx, &hy))
+    if (keys[SDL_SCANCODE_RIGHT])
+    {
+        if (keys[SDL_SCANCODE_LALT])
         {
-            player.y -= sensey - hy;
+            dx += PlayerMoveSpeed * cos(player.a - Pi / 2.0f);
+            dy += PlayerMoveSpeed * sin(player.a - Pi / 2.0f);
+        }
+        else player.a -= PlayerRotateSpeed;
+    }
+
+    if (keys[SDL_SCANCODE_UP])
+    {
+        dx += PlayerMoveSpeed * cos(player.a);
+        dy += PlayerMoveSpeed * sin(player.a);
+    }
+
+    if (keys[SDL_SCANCODE_DOWN])
+    {
+        dx -= PlayerMoveSpeed * cos(player.a);
+        dy -= PlayerMoveSpeed * sin(player.a);
+    }
+
+    const float Margin = 0.2f;
+
+    const int ix = static_cast<int>(player.x);
+    const int iy = static_cast<int>(player.y);
+    const int nx = ix + (dx > 0.0f ? 1 : -1);
+    const int ny = iy + (dy > 0.0f ? 1 : -1);
+
+    if (map(nx, iy))
+    {
+        if (dx > 0.0f)
+        {
+            const float wallx = nx - Margin;
+            if (player.x + dx >= wallx)
+                player.x = wallx;
+            else player.x += dx;
+        }
+        else if (dx < 0.0f)
+        {
+            const float wallx = nx + 1 + Margin;
+            if (player.x + dx <= wallx)
+                player.x = wallx;
+            else player.x += dx;
         }
     }
+    else player.x += dx;
+
+    if (map(ix, ny))
+    {
+        if (dy > 0.0f)
+        {
+            const float wally = ny - Margin;
+            if (player.y + dy >= wally)
+                player.y = wally;
+            else player.y += dy;
+        }
+        else if (dy < 0.0f)
+        {
+            const float wally = ny + 1 + Margin;
+            if (player.y + dy <= wally)
+                player.y = wally;
+            else player.y += dy;
+        }
+    }
+    else player.y += dy;
 }
 
 void render(ScreenPixel* pixels)
